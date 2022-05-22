8260fe53a0e61bfb28e4a53343c755bdf097f16d
Frederico Meira <fmeira@c1r14s3.42lisboa.com>
2022-05-22 20:35:26 +0100

    ex02 done, maybe needs other tests

--- ex02/includes/Array.hpp
+++ ex02/includes/Array.hpp
@@ -5,24 +5,54 @@
 #ifndef __ARRAY_HPP__
 # define __ARRAY_HPP__
 
-#include <iostream>
-
-template <class T>
+template <typename T>
 class Array {
 	public:
-		Array(){arr_ = new (T);};
-		Array(T n){ arr_ = new (T[n]);};
-		Array(const Array& a){if (a.arr_){for (int i = 0; i < a.size(); i++){this->arr_[i] = a.arr_[i];}}};
-		~Array();
+		Array() : arr_(NULL), size_(0) {}
+		Array(unsigned int n): arr_(new T[n]), size_(n) {}
+		Array(Array const &copy): arr_(NULL) { *this = copy; }
+		~Array() {delete[] arr_;};
 
-		Array&	operator= (const Array&){if (a.arr_){for (int i = 0; i < a.size(); i++){this->arr_[i] = a.arr_[i];}}return(*this);};
-		~Array();
-		const Array& operator[](std::size_t idx){try: this.arr_[idx]; catch(...);throw(std::exception);return this.arr_[idx]; }
+		unsigned int size(void) const
+		{
+			return (this->size_);
+		};
 
-		std::size_t	size(void){int i = 0; while (this->arr_[i]){i++;}return(i);}
+		Array &operator=(Array &a)
+		{
+			if (this == &a)
+				return (*this);
 
-		T*	arr_;
+			delete[] arr_;
+			size_ = a.size();
+			arr_ = new T[size_];
+			for (unsigned int i = 0; i < size_; i++)
+				this->arr_[i] = a.arr_[i];
+
+			return (*this);
+		}
+
+		T &operator[](unsigned int i)
+		{
+			if (i < 0 || i >= size_)
+				throw (Array::IndexOutofBounds());
+			return (arr_[i]);
+		}
+
+
+		class IndexOutofBounds : public std::exception
+		{
+			public:
+				const char* what() const throw() {
+					return ("Index out of bounds");
+				}
+		};
+
+		private:
+			T*				arr_;
+			unsigned int	size_;
 };
+
 // std::ostream&	operator<<(std::ostream&, const Array&);
 #endif
 
